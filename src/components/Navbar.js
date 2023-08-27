8d0d10ff4fd7c533f8653e962d1fe7c10d64ddc2
Edward Ng <ehng359@ucla.edu>
2023-08-26 18:14:30 -0700

    Adjusted folder directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,11 @@ export default function Navbar () {
     return (
         <div>
             <div style={{backgroundColor: "lightgrey", height:"8vh", margin: "auto"}}>
-                <div style={{display: 'flex', float: "left", backgroundColor: "white", width: "15vw", height: "8vh"}}>
-                    <img src="../../placeholder_image.jpeg" height="50px" width="50px" style={{position: "relative", margin: "auto"}}/>
+                <div style={{display: 'flex', float: "left", backgroundColor: "red", width: "15vw", height: "8vh"}}>
                     <NavLink to="/" style={{fontSize:24, margin: "auto"}}>Edward Ng</NavLink>
+                    <svg width="38" height="32" viewBox="0 0 38 32" fill="none" xmlns="http://www.w3.org/2000/svg" style={{backgroundColor: "gray"}}>
+                        <path d="M25.513 2H2V13.168M2 13.168H25.2154M2 13.168V23.6019H25.2154M2.35716 3.51797L23.9653 24.9447V3.51797M35.9302 7.69225C35.9302 5.53735 27.2082 3.51896 27.2082 11.8321C27.2082 20.6624 35.1032 17.7755 35.9302 10.2874M35.9302 7.69225C36.0264 8.60557 36.02 9.47367 35.9302 10.2874M35.9302 7.69225V10.2874M35.9302 10.2874V23.2897C35.7254 28.1876 32.8672 35.1905 23.0724 24.0186" stroke="black" stroke-width="3"/>
+                    </svg>
                 </div>
                 <div style={{display: "flex", float: "right", backgroundColor: "grey", justifyContent:"center", width: "30vw", height: "8vh"}}>
                     <NavLink to="projects" style={{margin:"auto"}}>
